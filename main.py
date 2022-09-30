93f7a45de5a8eb02304e3edee4b382f68a928de6
CodeWracker <dev.rodrigofs@gmail.com>
2022-09-30 19:36:24 -0300

    regras de pedágio corrigidas para paralelas, não opções

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dateutil.relativedelta import relativedelta
 from pprint import pprint
 
 
-def read_csv(path):
-    return pd.read_csv(path)
+def read_csv(path_militares,path_tipo_tempo,path_militar_tempo):
+    return pd.read_csv(path_militares),pd.read_csv(path_tipo_tempo),pd.read_csv(path_militar_tempo)
 
 
 def summarize_data(df):
@@ -191,7 +191,7 @@ def get_city_data(df):
     return cidade_lotacao
 
 
-def calc_retirement(gender, dt_entry, days_before):
+def calc_retirement(gender, dt_entry,ids_tipo, qtds_dias,df_tipo_tempo):
     '''
     Homens
         - Se sua data de ingresso é até 19/12/2013: deve ter 35 anos de serviço, não tem exigência de tempo militar, pode somar todo o tempo que tiver fora.
@@ -207,8 +207,89 @@ def calc_retirement(gender, dt_entry, days_before):
             Se até 31/12/2021 tiver fechado as regras anteriores, não há pedágio. Caso contrário, do tempo que restar para fechar o tempo militar em 01/01/2022 deve ser acrescentado 4 meses para cada ano faltante.
     '''
     #print(str(dt_entry))
+    ped_flag17,ped_flag4m = False,False
     dt_entry_datetime = datetime.strptime(str(dt_entry), "%Y-%m-%d")
     remaing_time = timedelta(days=0)
+    if (gender == "Masculino"):
+        if (dt_entry_datetime <= datetime.strptime("2013-12-19", "%Y-%m-%d")):
+            d_t_ant = timedelta(days=0)
+            for i,tipo in enumerate(ids_tipo):
+                d_t_ant = d_t_ant - timedelta(days=int(qtds_dias[i]))
+            
+            remaing_time = timedelta(days=(35*365)) - ((datetime.now() - dt_entry_datetime) - d_t_ant) 
+            if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
+                ped_flag17 = True
+                remaing_time += timedelta(days=(remaing_time.days*0.17))
+        if (dt_entry_datetime > datetime.strptime("2013-12-19", "%Y-%m-%d")):
+            mili_time = 0
+            n_mili_time = 0
+            for i,tipo in enumerate(ids_tipo):
+                if(df_tipo_tempo[df_tipo_tempo['id_tipo'] == tipo]['is_militar'].values[0] == 1):
+                    mili_time += qtd_dias[i]
+                else:
+                    n_mili_time += qtd_dias[i]
+            if (n_mili_time/365 > 5):
+                n_mili_time = 5*365
+            
+            remaing_time =timedelta(days=(35*365)) - ( (datetime.now() - dt_entry_datetime) - timedelta(days=mili_time[i]) - timedelta(days=n_mili_time[i]))
+            if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
+                ped_flag17 = True
+                remaing_time += timedelta(days=(remaing_time.days*0.17))
+                if(mili_time<30*365):
+                    ped_flag4m = True
+                    remaing_time += relativedelta(months=4)
+    
+    else:
+        if (dt_entry_datetime <= datetime.strptime("2006-08-31", "%Y-%m-%d")):
+            d_t_ant = timedelta(days=0)
+            for i,tipo in enumerate(ids_tipo):
+                d_t_ant = d_t_ant - timedelta(days=int(qtds_dias[i]))
+            
+            remaing_time = timedelta(days=(35*365)) - ((datetime.now() - dt_entry_datetime) - d_t_ant)
+            if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
+                ped_flag17 = True
+                remaing_time += timedelta(days=(remaing_time.days*0.17))
+        if (dt_entry_datetime >= datetime.strptime("2006-09-01", "%Y-%m-%d") and dt_entry_datetime <= datetime.strptime("2013-12-19", "%Y-%m-%d")):
+            remaing_time = timedelta(days=(35*365))
+            mili_time = 0
+            n_mili_time = 0
+            for i,tipo in enumerate(ids_tipo):
+                if(df_tipo_tempo[df_tipo_tempo['id_tipo'] == tipo]['is_militar'].values[0] == 1):
+                    mili_time += qtd_dias[i]
+                else:
+                    n_mili_time += qtd_dias[i]
+            if (n_mili_time/365 > 15):
+                n_mili_time = 15*365
+            remaing_time =timedelta(days=(35*365)) - ( (datetime.now() - dt_entry_datetime) - timedelta(days=mili_time[i]) - timedelta(days=n_mili_time[i]))
+            if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
+                ped_flag17 = True
+                remaing_time += timedelta(days=(remaing_time.days*0.17))
+                if(mili_time<20*365):
+                    ped_flag4m = True
+                    remaing_time += relativedelta(months=4)
+
+        
+        if (dt_entry_datetime > datetime.strptime("2013-12-19", "%Y-%m-%d")):
+            remaing_time = timedelta(days=(35*365))
+            mili_time = 0
+            n_mili_time = 0
+            for i,tipo in enumerate(ids_tipo):
+                if(df_tipo_tempo[df_tipo_tempo['id_tipo'] == tipo]['is_militar'].values[0] == 1):
+                    mili_time += qtd_dias[i]
+                else:
+                    n_mili_time += qtd_dias[i]
+            if (n_mili_time/365 > 5):
+                n_mili_time = 5*365
+            remaing_time =timedelta(days=(35*365)) - ( (datetime.now() - dt_entry_datetime) - timedelta(days=mili_time[i]) - timedelta(days=n_mili_time[i]))
+            if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
+                ped_flag17 = True
+                remaing_time += timedelta(days=(remaing_time.days*0.17))
+                if(mili_time<30*365):
+                    ped_flag4m = True
+                    remaing_time += relativedelta(months=4)
+
+
+    '''
     if (gender == "Masculino"):
         if (dt_entry_datetime <= datetime.strptime("2013-12-19", "%Y-%m-%d")):
             # os 35 anos de serviço aqui contam trabalhos anteriores?
@@ -236,43 +317,51 @@ def calc_retirement(gender, dt_entry, days_before):
     if(datetime.now() + remaing_time>datetime.strptime("2021-12-31", "%Y-%m-%d")):
         ped["pedagio 17%"] = ( remaing_time + timedelta(days = (remaing_time.days*0.17))).days
         ped["pedagio 4 meses"] = ( remaing_time + relativedelta(months=+4)).days
+    '''
 
-    return datetime.now() + remaing_time, ped
+    return datetime.now() + remaing_time, {"pedagio 17%": ped_flag17, "pedagio 4 meses": ped_flag4m}
 
 
-def analyse_retirement(df):
-    nomes, matriculas = df['nome'], df['matrícula']. astype(int)
+def analyse_retirement(df_militares,df_tipo_tempo,df_tempo_militares):
+    nomes, matriculas = df_militares['nome'], df_militares['matrícula']. astype(int)
     rets_remaining_time = []
     ret_data = []
     for mat in matriculas:
-        line = df[df['matrícula'] == mat]
+        line = df_militares[df_militares['matrícula'] == mat]
         sx = line['sexo'].values[0]
         #print(sx)
+        ids_tipo, qtds_dias = [], []
+        tipo_tempo = df_tempo_militares[df_tempo_militares['matricula_militar'] == mat]
+        if(len(tipo_tempo) > 0):
+            for(_, row) in tipo_tempo.iterrows():
+                ids_tipo.append(row['id_tipo'])
+                qtds_dias.append(row['tempo_dias'])
+            #print(ids_tipo, qtds_dias)
         dt_entry = line['data de ingresso'].values[0]
         days_before = line['tempo anterior_tempo em dias'].values[0]
-        ret_prev,ped = calc_retirement(sx, dt_entry,  days_before)
+        ret_prev,ped = calc_retirement(sx,dt_entry, ids_tipo, qtds_dias,df_tipo_tempo)
         
         data = {
             "Nome": line['nome'].values[0],
             "Matricula": mat,
-            "Data Prevista para aposentadoria (sem pedágio)": f'{ret_prev.day}/{ret_prev.month}/{ret_prev.year}', 
-            "Opções de Pedágio (em dias)": ped}
+            "Data Prevista para aposentadoria (com pedágio)": f'{ret_prev.day}/{ret_prev.month}/{ret_prev.year}', 
+            "Pedágios aplicados": ped}
         ret_data.append(data)
     return ret_data
 
 
 def main():
-    df = read_csv("banco_de_pessoal.csv")
+    df_militares,df_tipo_tempo,df_tempo_militares = read_csv("banco_de_pessoal.csv","tipo_tempo.csv","militar_tipo_tempo.csv")
     print("1 - Ver dados sumarizados")
     print("2 - Ver dados de aposentadoria")
     choice = int(input("Escolha uma opção: "))
     if(choice == 1):
         print("Aguarde, processando...")
-        summarized_dt = summarize_data(df)
+        summarized_dt = summarize_data(df_militares)
         pprint(summarized_dt)
     if(choice == 2):
         print("Aguarde, processando...")
-        ret_data = analyse_retirement(df)
+        ret_data = analyse_retirement(df_militares,df_tipo_tempo,df_tempo_militares)
         print("1 - ver todos os dados")
         print("2 - ver dados de um militar específico")
         choice = int(input("Escolha uma opção: "))
@@ -285,7 +374,7 @@ def main():
                 if(data["Matricula"] == mat):
                     pprint(data)
                     break
-    rets_dt = analyse_retirement(df)
+    rets_dt = analyse_retirement(df_militares,df_tipo_tempo,df_tempo_militares)
     
 
 
